8702e44b4207eee1d319c2d8c50f47d6b88e01af
thanatus <s.sidoryaka@gmail.com>
2021-01-07 17:43:17 +0200

    add page 2-3

--- js/jscript.js
+++ js/jscript.js
@@ -1,18 +1,18 @@
 console.log('Work');
 
-if (
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-    navigator.userAgent
-  )
-) {
-  // код для мобильных устройств
-  const viber = document.querySelector('#viber');
-  console.log('Mobila');
-  viber.href = 'viber://add?number=380503283636';
-} else {
-  // код для обычных устройств
-  console.log('PC');
-}
+// if (
+//   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+//     navigator.userAgent
+//   )
+// ) {
+//   // код для мобильных устройств
+//   const viber = document.querySelector('#viber');
+//   console.log('Mobila');
+//   viber.href = 'viber://add?number=380503283636';
+// } else {
+//   // код для обычных устройств
+//   console.log('PC');
+// }
 
 let pageSlider = new Swiper('.page', {
   wrapperClass: 'page__wrapper',
